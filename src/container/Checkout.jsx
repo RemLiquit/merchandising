090da24d0891c1cffbe87fe0768c4adbe338ab1e
Dyego <ceixpp@gmail.com>
2021-01-28 16:03:29 -0400

    [add] react-helmet

--- src/container/Checkout.jsx
+++ src/container/Checkout.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import { Helmet } from "react-helmet";
 import { Link } from "react-router-dom";
 import AppContext from "../context/AppContext";
 import "../styles/container/Checkout.css";
@@ -19,36 +20,45 @@ const Checkout = () => {
   };
 
   return (
-    <div className="Checkout">
-      <div className="Checkout-content">
-        {cart.length > 0 ? (
-          <h3>Lista de Articulos:</h3>
-        ) : (
-          <h3>Sin pedidos...</h3>
-        )}
-        {cart.map((item) => (
-          <div className="Checkout-item">
-            <div className="Checkout-element">
-              <div style={{ display: "flex", alignItems: "center" }}>
-                <img src={item.image} alt="image" className="Checkout-image" />
-                <h4>&nbsp; {item.title}</h4>
+    <React.Fragment>
+      <Helmet>
+        <title>Lista de Pedidos - Praxi-store Merch</title>
+      </Helmet>
+      <div className="Checkout">
+        <div className="Checkout-content">
+          {cart.length > 0 ? (
+            <h3>Lista de Articulos:</h3>
+          ) : (
+            <h3>Sin pedidos...</h3>
+          )}
+          {cart.map((item) => (
+            <div className="Checkout-item">
+              <div className="Checkout-element">
+                <div style={{ display: "flex", alignItems: "center" }}>
+                  <img
+                    src={item.image}
+                    alt="image"
+                    className="Checkout-image"
+                  />
+                  <h4>&nbsp; {item.title}</h4>
+                </div>
+                <span>${item.price}</span>
               </div>
-              <span>${item.price}</span>
+              &nbsp;{" "}
+              <i className="far fa-trash-alt" onClick={handleRemove(item)} />
             </div>
-            &nbsp;{" "}
-            <i className="far fa-trash-alt" onClick={handleRemove(item)} />
-          </div>
-        ))}
-      </div>
-      {cart.length > 0 && (
-        <div className="Checkout-sidebar">
-          <h3>{`Precio Total: $ ${handleSumTotal()}`}</h3>
-          <Link to="/checkout/information">
-            <button type="button">Continuar pedido</button>
-          </Link>
+          ))}
         </div>
-      )}
-    </div>
+        {cart.length > 0 && (
+          <div className="Checkout-sidebar">
+            <h3>{`Precio Total: $ ${handleSumTotal()}`}</h3>
+            <Link to="/checkout/information">
+              <button type="button">Continuar pedido</button>
+            </Link>
+          </div>
+        )}
+      </div>
+    </React.Fragment>
   );
 };
 
--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -1,12 +1,36 @@
 import React from "react";
 import Products from "../components/Products";
 import initialState from "../initialState";
+import { Helmet } from "react-helmet";
 
 const Home = () => {
   return (
-    <div>
+    <React.Fragment>
+      <Helmet>
+        <title>Praxi-store Merch</title>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@TU_USER" />
+        <meta name="twitter:creator" content="@TU_USER" />
+        <meta name="twitter:title" content="Praxi-Store Merch" />
+        <meta name="twitter:description" content="Praxi-Store Merch" />
+        <meta
+          name="twitter:image"
+          content="https://s3.amazonaws.com/gndx.dev/gndxdev.png"
+        />
+        <meta property="og:title" content="Praxi-Store Merch" />
+        <meta property="og:description" content="Praxi-Store Merch" />
+        <meta
+          property="og:image"
+          content="https://s3.amazonaws.com/gndx.dev/gndxdev.png"
+        />
+        <meta property="og:url" content="praxi-store.xyz" />
+        <meta property="og:site_name" content="Praxi-Store Merch" />
+        <meta property="og:locale" content="es_ES" />
+        <meta property="og:type" content="article" />
+        <meta property="fb:app_id" content="ID_APP_FACEBOOK" />
+      </Helmet>
       <Products products={initialState.products} />
-    </div>
+    </React.Fragment>
   );
 };
 
